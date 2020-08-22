5542bc56120fb1b512ce5a5f1db2dbb1392d8cd9
Ashish Kumar <60195105+ashishcssom@users.noreply.github.com>
2020-08-23 00:48:54 +0530

    corrected config file

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-import os
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".")).replace("\\", "/")
-prototxtPath=path+r"/models/deploy.prototxt" # Face detector model
-weightsPath=path+r"/models/res10_300x300_ssd_iter_140000.caffemodel" # Trained face mask detector model
+prototxtPath="./models/deploy.prototxt" # Face detector model
+weightsPath="./models/res10_300x300_ssd_iter_140000.caffemodel" # Trained face mask detector model
 confidence=0.5
-model=path+r"/models/mask_detector.model"
-fps=35
+model="./models/mask_detector.model"
 
